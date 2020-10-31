cb6c2f01a7584572a3490ed1e7f218ecabc7b2be
kanta <kanta.munton@gmail.com>
2020-10-31 17:31:46 +0000

    added scroll to top component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Join from './components/Join/Join'
 import Game from './components/Game/Game'
 import Footer from './components/footer/Footer'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 const App = () => (
     <Router>
-        <Switch>
+        <ScrollToTop>
+         <Switch>
             <Route path="/" exact component={Join} />
             <Route path="/join" component={Join} />
             <Route path="/game" component={Game} />
-        </Switch> 
+        </Switch>   
+        </ScrollToTop>
+         
         <Footer />
     </Router>
 
