463709b7f65a1108fff6557a5c349d1e898f0972
kanta <kanta.munton@gmail.com>
2020-11-14 14:16:35 +0000

    how to play button slightly lower on computer screen full screen view

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -161,7 +161,7 @@ const Room = ({
                 </div>
                 <div className="info row justify-content-start">
                     <div className="col-lg-6 text-center">
-                        <button type="button" className="btn btn-primary btn-sm" data-toggle="modal" data-target="#howToPlay">
+                        <button type="button" className="btn btn-primary btn-sm mt-1" data-toggle="modal" data-target="#howToPlay">
                             How to play
                         </button>
                         <Modal />
