56be336073eeede8c8e3e021c92353863c434eab
Kanta Munton <kanta.munton@gmail.com>
2023-08-02 12:36:49 +0100

    Add a buy me a coffee link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,6 +11,9 @@ class Footer extends React.Component {
 			<footer id="footer" className="container">
 				<div className="row justify-content-center">
 					<div className="col-12 text-center">
+						<a href="https://www.buymeacoffee.com/kmunton" target="_blank">
+							<img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: '50px'}} />
+						</a>
 						<p id="footer_text">
 							Created by KMunton<br />
 							<FontAwesomeIcon icon={faCopyright} /> {x}
