ce522a55de04feb68dbb45ba05218fa77b3f866d
Kanta Munton <kanta.munton@gmail.com>
2023-08-02 11:45:24 +0100

    Replace process.env with import.meta.env

--- src/components/ServiceUnavailableBanner/ServiceUnavailableBanner.jsx
+++ src/components/ServiceUnavailableBanner/ServiceUnavailableBanner.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ServiceUnavailableBanner = () => (
     <div className="alert alert-info text-center" role="alert">
         <h1 className="alert-heading">Update</h1>
-        <p className="lead">{process.env.VITE_APP_MAIN_REASON}</p>
-        <p className="lead">{process.env.VITE_APP_MORE_DETAIL}</p>
+        <p className="lead">{import.meta.env.VITE_APP_MAIN_REASON}</p>
+        <p className="lead">{import.meta.env.VITE_APP_MORE_DETAIL}</p>
         <p className="lead">Source code is available on <a href="https://github.com/Panda4817/Multiplayer-Pictionary-Frontend" class="alert-link">GitHub</a> if you wish to run it yourself.</p>
         <p className="lead">Sorry for the inconvenience.</p>
     </div>
